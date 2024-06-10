cd2610940d7ca62b31e75c4297a3985bce3af799
Margo <bushmargarita4@gmail.com>
2024-06-10 14:31:07 +0300

    edit alembic/env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,7 @@
+import asyncio
+
 from logging.config import fileConfig
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncAdaptedPool
 
 from alembic import context
 from models import Base
@@ -33,18 +33,12 @@ def run_migrations_offline():
 # Функция для выполнения миграций в онлайн-режиме.
 async def run_migrations_online():
     # Создаем подключаемый объект к базе данных, используя настройки из конфигурации.
-    connectable = AsyncEngine(
-        engine_from_config(
-            config.get_section(config.config_ini_section),
-            prefix="sqlalchemy.",
-            poolclass=pool.NullPool,
-        )
-    )
+    connectable = create_async_engine(config.get_main_option("sqlalchemy.url"), echo=True, future=True, poolclass=AsyncAdaptedPool)
 
     # Устанавливаем соединение с базой данных.
     async with connectable.connect() as connection:
         # Настраиваем контекст миграций для онлайн-режима.
-        await connection.run_sync(do_run_migrations)
+        await do_run_migrations(connection)
 
 async def do_run_migrations(connection):
     context.configure(connection=connection, target_metadata=target_metadata)
@@ -57,5 +51,4 @@ async def do_run_migrations(connection):
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-    import asyncio
     asyncio.run(run_migrations_online())
