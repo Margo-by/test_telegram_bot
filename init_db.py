6360b136c12a4fcf9c9819f1a2b8cfa7184f570a
Margo <bushmargarita4@gmail.com>
2024-06-10 12:47:27 +0300

    edit init_db.py,requirements.txt

--- init_db.py
+++ init_db.py
@@ -12,10 +12,9 @@ DATABASE_URL = settings.DATABASE_URL
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-engine = create_async_engine(DATABASE_URL)
-
 async def init_db():
     logger.info("Инициализация базы данных...")
+    engine = create_async_engine(DATABASE_URL, echo=True, future=True, pool_pre_ping=True)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     logger.info("База данных инициализирована успешно.")
